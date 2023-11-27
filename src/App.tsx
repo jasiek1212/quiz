a6a63d0c11b5b1ef51c62a301e0aa0e457528556
Karol Kosek <karol.kosek@deployed.pl>
2023-11-27 14:19:56 +0100

    Styles - setup tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,18 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+    <main className="flex flex-col gap-y-4">
+      <h1 className="font-bold text-3xl">Vite + React</h1>
+      <div className="flex flex-col gap-x-2">
+        <button className="w-24" onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <p>Click on the Vite and React logos to learn more</p>
+    </main>
   );
 }
 
