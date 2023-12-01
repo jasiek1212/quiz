dda19e145e7754ea1832c793b22be08a0716995b
Karol Kosek <karol.kosek@deployed.pl>
2023-12-01 11:01:53 +0100

    Change node to the newest one and add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { useState } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Start from "./pages/Start";
+import Quiz from "./pages/Quiz";
+import Results from "./pages/Results";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Start />,
+  },
+  {
+    path: "/quiz",
+    element: <Quiz />,
+  },
+  {
+    path: "/results",
+    element: <Results />,
+  },
+]);
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <main className="flex flex-col gap-y-4">
-      <h1 className="font-bold text-3xl">Vite + React</h1>
-      <div className="flex flex-col gap-x-2">
-        <button className="w-24" onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>Click on the Vite and React logos to learn more</p>
-    </main>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
